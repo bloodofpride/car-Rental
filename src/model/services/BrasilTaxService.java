0c94bbdd85807b5ab30983b775183bf90386c77a
bloodofpride <antonio_maxwell@hotmail.com>
2022-01-26 08:37:48 -0300

    solution with interface

--- src/model/services/BrasilTaxService.java
+++ src/model/services/BrasilTaxService.java
@@ -1,6 +1,6 @@
 package model.services;
 
-public class BrasilTaxService {
+public class BrasilTaxService implements TaxService{
 
     public double tax(double amount){
         return amount <= 100 ? amount * 0.2 : amount * 0.15;
